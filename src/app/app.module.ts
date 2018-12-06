bedd553cc659832b9630324bd2fa4ddc80aa2c58
tom@tom.cz <tom@tom.cz>
2018-12-07 00:56:22 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsertableComponent
   ],
   imports: [
     BrowserModule

3c6366f7d064df73d6ff88374986319094e2f00a
tom@tom.cz <tom@tom.cz>
2018-12-23 18:43:27 +0100

    zacatek menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTableModule,MatPaginatorModule,MatFormFieldModule,MatInputModule, MatSortModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UsertableComponent } from './components/usertable/usertable.component';
 import { TableOverviewExample } from './components/tableStack/table-overview-example';
 import { UserService } from './services/user.service';
+import { ApListComponent } from './components/ap/ap-list/ap-list.component';
+import { RpListComponent } from './components/rp/rp-list/rp-list.component';
+import { WelcomeComponent } from './components/welcome/welcome/welcome.component';
 
 
 @NgModule({
@@ -15,6 +19,7 @@ import { UserService } from './services/user.service';
     AppComponent,
     UsertableComponent,
     TableOverviewExample,
+    ApListComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,15 @@ import { UserService } from './services/user.service';
     MatPaginatorModule,
     MatFormFieldModule,
     MatInputModule,
-    MatSortModule
+    MatSortModule,
+    RouterModule.forRoot([
+      {path: 'users', component:UsertableComponent},
+      {path: 'aps', component:AppComponent},
+      {path: 'rps', component:RpListComponent},
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
---
+++ src/app/components/rp/rp-list/rp-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rp-list',
+  templateUrl: './rp-list.component.html',
+  styleUrls: ['./rp-list.component.css']
+})
+export class RpListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
